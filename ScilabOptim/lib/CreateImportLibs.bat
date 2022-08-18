3141401775a93f74d30d2267a33eff0b283c6faf
pjumppanen <paavo.jumppanen@csiro.au>
2022-08-18 02:04:42 +0100

    Add def and script to make import library for MSVC

---
+++ ScilabOptim/lib/CreateImportLibs.bat
@@ -0,0 +1,3 @@
+REM create R import library to interface with R.dll
+LIB /DEF:ScilabOptim.def /OUT:.\i386\ScilabOptim.lib /MACHINE:X86
+LIB /DEF:ScilabOptim.def /OUT:.\x64\ScilabOptim.lib /MACHINE:X64
