c36973110f6258982d6c62c27f1a385f4b5bc94e
pjumppanen <paavo.jumppanen@csiro.au>
2022-08-19 10:05:19 +0100

    Add means of installing dll's and creating VS import libs

--- ScilabOptim/lib/CreateImportLibs.bat
+++ ScilabOptim/lib/CreateImportLibs.bat
@@ -1,3 +1,11 @@
 REM create R import library to interface with R.dll
-LIB /DEF:ScilabOptim.def /OUT:.\i386\ScilabOptim.lib /MACHINE:X86
-LIB /DEF:ScilabOptim.def /OUT:.\x64\ScilabOptim.lib /MACHINE:X64
+REM setup VS environment
+SET bat_path=%~dp0
+for /f "usebackq delims=#" %%a in (`"%programfiles(x86)%\Microsoft Visual Studio\Installer\vswhere" -latest -property installationPath`) do set VsDevCmd_Path=%%a\Common7\Tools\VsDevCmd.bat
+IF "%FrameworkVersion%"=="" "%VsDevCmd_Path%"
+
+REM create VS import libs
+LIB /DEF:ScilabOptim.def /OUT:%bat_path%..\..\objs\win32\debug\ScilabOptim.lib /MACHINE:X86
+LIB /DEF:ScilabOptim.def /OUT:%bat_path%..\..\objs\win32\release\ScilabOptim.lib /MACHINE:X86
+LIB /DEF:ScilabOptim.def /OUT:%bat_path%..\..\objs\x64\debug\ScilabOptim.lib /MACHINE:X64
+LIB /DEF:ScilabOptim.def /OUT:%bat_path%..\..\objs\x64\release\ScilabOptim.lib /MACHINE:X64
